a07747d0c1774302db5852ca9176c72af0deb2b0
steph <stephane@henriod.info>
2025-02-28 16:46:29 +0100

    Adding the SAP topics

--- api/get_indicators.php
+++ api/get_indicators.php
@@ -7,26 +7,41 @@ $query = "
     SELECT i.indicator_id, i.name, i.definition, i.unit_of_measurement, i.disaggregation, 
            i.data_collection_method, i.underlying_theory_of_change, i.indicator_level,
            CASE 
-		   WHEN i.is_standard = 'FALSE' THEN 'no'
-		   ELSE 'BMZ Standard indicator'
-		   END as is_standard, 
-		   i.additional_info,
-           t.name AS theme_name, t.description AS theme_description, s.name AS sdg_name, s.sdg_id AS sdg_id, s.description as sdg_description, s.url AS sdg_url,
-           GROUP_CONCAT(c.name || '|' || c.definition, '||') AS concepts,
-           GROUP_CONCAT(p.name || '|' || p.definition, '||') AS packages,
+               WHEN i.is_standard = 'FALSE' THEN 'no'
+               ELSE 'BMZ Standard indicator'
+           END as is_standard, 
+           i.additional_info,
+           t.name AS theme_name, t.description AS theme_description, 
+           s.name AS sdg_name, s.sdg_id AS sdg_id, s.description AS sdg_description, s.url AS sdg_url,
+           
+           (SELECT GROUP_CONCAT(c.name || '|' || c.definition, '||') 
+            FROM indicator_concepts ic
+            JOIN key_concepts c ON ic.concept_id = c.concept_id
+            WHERE ic.indicator_id = i.indicator_id) AS concepts,
+
+           (SELECT GROUP_CONCAT(p.name || '|' || p.definition, '||') 
+            FROM indicator_packages ip
+            JOIN packages p ON ip.package_id = p.package_id
+            WHERE ip.indicator_id = i.indicator_id) AS packages,
+
+           (SELECT GROUP_CONCAT(sp.topic_name, '||') 
+            FROM indicator_saptopics isap
+            JOIN sap_topics sp ON isap.topic_id = sp.topic_id
+            WHERE isap.indicator_id = i.indicator_id) AS sap_topics,
+
            sec.name AS sectors, sec.description AS sectors_description,
            u.unit_shortname AS unit_shortname, u.unit_longname AS unit_longname
+           
     FROM indicators i
     JOIN themes t ON i.theme_id = t.theme_id
     JOIN sdgs s ON i.sdg_id = s.sdg_id
-    LEFT JOIN indicator_concepts ic ON i.indicator_id = ic.indicator_id
-    LEFT JOIN indicator_packages ip ON i.indicator_id = ip.indicator_id
-    LEFT JOIN key_concepts c ON ic.concept_id = c.concept_id
-    LEFT JOIN packages p ON ip.package_id = p.package_id
     LEFT JOIN sectors sec ON t.sector_id = sec.sector_id
     LEFT JOIN units u ON i.responsible_unit = u.unit_id
+
     GROUP BY i.indicator_id
     ORDER BY sec.sector_id, t.name, i.name;
+
+
 ";
 
 $stmt = $db->prepare($query);
